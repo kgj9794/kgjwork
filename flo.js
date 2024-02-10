5f026b8e321acc32f5b169f6fe04d9b4cb209473
김건주 <49966040+kgj9794@users.noreply.github.com>
2024-02-10 22:30:36 +0900

    첫 업로드
    ㅇ

---
+++ flo.js
@@ -0,0 +1,44 @@
+/*토스트 알림*/
+let removeToast;
+
+function toast(string) {
+    const toast = document.getElementById("toast");
+
+    toast.classList.contains("reveal") ?
+        (clearTimeout(removeToast), removeToast = setTimeout(function () {
+            document.getElementById("toast").classList.remove("reveal")
+        }, 8000)) :
+        removeToast = setTimeout(function () {
+            document.getElementById("toast").classList.remove("reveal")
+        }, 8000)
+    toast.classList.add("reveal"),
+        toast.innerText = string
+}
+
+function toast2(string) {
+    const toast2 = document.getElementById("toast2");
+
+    toast2.classList.contains("reveal") ?
+        (clearTimeout(removeToast), removeToast = setTimeout(function () {
+            document.getElementById("toast2").classList.remove("reveal")
+        }, 3000)) :
+        removeToast = setTimeout(function () {
+            document.getElementById("toast2").classList.remove("reveal")
+        }, 3000)
+    toast2.classList.add("reveal"),
+        toast2.innerText = string
+}
+
+function toast3(string) {
+    const toast3 = document.getElementById("toast3");
+
+    toast3.classList.contains("reveal") ?
+        (clearTimeout(removeToast), removeToast = setTimeout(function () {
+            document.getElementById("toast3").classList.remove("reveal")
+        }, 5000)) :
+        removeToast = setTimeout(function () {
+            document.getElementById("toast3").classList.remove("reveal")
+        }, 5000)
+    toast3.classList.add("reveal"),
+        toast3.innerText = string
+}
